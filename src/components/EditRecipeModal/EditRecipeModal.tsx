f82f193502bc23ab7326403777583430050e9d78
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-02 14:58:52 -0400

    Create: EditRecipeModal component

---
+++ src/components/EditRecipeModal/EditRecipeModal.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const EditRecipeModal = () => {
+  return <p>EditRecipeModal</p>
+}
+
+export default EditRecipeModal;
---
+++ src/components/EditRecipeModal/index.tsx
@@ -0,0 +1,1 @@
+export { default as EditRecipeModal } from './EditRecipeModal';
