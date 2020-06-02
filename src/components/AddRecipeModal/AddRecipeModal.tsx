ad337e1b0c231b7130cd6370f36e8e5aecbafc09
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-02 14:58:43 -0400

    Create: AddRecipeModal component

---
+++ src/components/AddRecipeModal/AddRecipeModal.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Styles
+import styles from './AddRecipeModal.module.scss'
+
+const AddRecipeModal = () => {
+  return <p>Hello</p>;
+};
+
+export default AddRecipeModal;
---
+++ src/components/AddRecipeModal/index.tsx
@@ -0,0 +1,1 @@
+export { default as AddRecipeModal } from './AddRecipeModal';
