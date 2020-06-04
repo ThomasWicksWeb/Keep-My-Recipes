5d8e2a4fb805465691b2ba59fe03465ea87a1252
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-04 19:06:53 -0400

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from "react";
 
-const Footer = () => {
-  return <p>Footer</p>
+import { ExternalLink } from "../ExternalLink";
+
+function AppFooter() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p className="is-size-6">
+          <strong>Keep My Recipes</strong> by{" "}
+          <ExternalLink
+            text="Thomas Wicks,"
+            href="https://thomaswicks.com/"
+            icon=""
+            className="text-highlight hvr-sweep-to-right has-text-weight-bold"
+          ></ExternalLink>{" "}
+          created as a personal project
+        </p>
+        <p className="is-size-6">
+          If you'd like to support this website, please consider my{" "}
+          <ExternalLink
+            text="Buy Me a Coffee"
+            href="https://www.buymeacoffee.com/thomaswicks"
+            icon=""
+            className="is-size-6 hvr-sweep-to-right text-highlight has-text-weight-bold"
+          ></ExternalLink>{" "}
+          ☕ page. Thank you!{" "}
+        </p>
+        <p className="is-size-6">
+          Curious about the code? Check out the{" "}
+          <ExternalLink
+            text="GitHub repository"
+            href="https://github.com/ThomasWicksWeb/Keep-My-Recipes"
+            icon=""
+            className="is-size-6 hvr-sweep-to-right text-highlight has-text-weight-bold"
+          ></ExternalLink>{" "}
+        </p>
+        <p className="is-size-6">
+          Site last updated <strong>May 26th, 2020</strong>
+        </p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer;
+export default AppFooter;
