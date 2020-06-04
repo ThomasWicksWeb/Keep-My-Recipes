225249e343ec6f31e9f14a0e1e5846cd1db38063
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-04 19:07:00 -0400

    Create AuthContext.jsx

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,22 @@
+import React, { createContext, useState, useEffect } from 'react';
+import firebase from 'firebase';
+
+export const AuthContext = createContext();
+
+const AuthContextProvider = (props) => {
+  const [userState, setAuthState] = useState();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setAuthState(user);
+    });
+  });
+
+  return (
+    <AuthContext.Provider value={{ userState }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
