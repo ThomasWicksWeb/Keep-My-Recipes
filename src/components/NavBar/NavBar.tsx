84ad1cfc7c53e99cca70103fdf009a1123b493dd
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-04 19:06:57 -0400

    Update NavBar.tsx

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,86 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+// Toast notifications
+import { toast, ToastContainer } from 'react-toastify';
+
+// Import Auth Context
+import { AuthContext } from '../../contexts/AuthContext';
+
+import firebase from 'firebase';
+import './NavBar.module.scss';
 
 const NavBar = () => {
-  return <p>NavBar</p>;
-};
+  const { userState } = useContext(AuthContext);
 
+  const history = useHistory();
+
+  // Handles user logout
+  const handleLogout = () => {
+    firebase.auth().signOut();
+    toast.success('Successfully logged out', {
+      position: 'top-center',
+    });
+    history.push('/login');
+  };
+
+  const AuthDependantButtons = () => {
+    if (!userState) {
+      return (
+        <div className="buttons">
+          <Link className="button is-info" to="/createaccount">
+            <strong>Sign Up</strong>
+          </Link>
+          <Link className="button is-light" to="/login">
+            <strong>Login</strong>
+          </Link>
+        </div>
+      );
+    } else {
+      return (
+        <>
+          <Link to="/notes" className="navbar-item">
+            <strong>My Notes</strong>
+          </Link>
+          <Link to="/account" className="navbar-item">
+            <strong>Account</strong>
+          </Link>
+          <button className="button is-info" onClick={handleLogout}>
+            <strong>Logout</strong>
+          </button>
+        </>
+      );
+    }
+  };
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <ToastContainer />
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item is-size-3 ScriptFont">
+          Keep My Notes
+        </Link>
+        <button
+          className="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </button>
+      </div>
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <Link to="/about" className="navbar-item">
+            <strong>About</strong>
+          </Link>
+        </div>
+        <div className="navbar-end">
+          <div className="navbar-item">{AuthDependantButtons()}</div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 export default NavBar;
