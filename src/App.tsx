4beffc29eeade31bdc9b4fb4cac93a421798389f
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-05 19:10:56 -0400

    Update: Wrapped app in ThemeContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ import "firebase/firestore";
 
 // Styling
 import "bulma"; // Styles app on a global level
-import 'react-toastify/dist/ReactToastify.css';
-
+import "react-toastify/dist/ReactToastify.css";
 
 // Contexts
 import AuthContextProvider from "./contexts/AuthContext";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 // Components
 import { Footer } from "./components/Footer";
@@ -23,8 +23,8 @@ import { Splash } from "./routes/Splash";
 import { Login } from "./routes/Login";
 import { CreateAccount } from "./routes/CreateAccount";
 import { Recipes } from "./routes/Recipes";
-import { Account } from './routes/Account'
-import { Settings } from './routes/Settings'
+import { Account } from "./routes/Account";
+import { Settings } from "./routes/Settings";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
@@ -48,18 +48,20 @@ export const auth = firebase.auth();
 function App() {
   return (
     <AuthContextProvider>
-      <BrowserRouter>
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={Splash} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/createaccount" component={CreateAccount} />
-          <Route exact path="/recipes" component={Recipes} />
-          <Route exact path="/account" component={Account} />
-          <Route exact path="/settings" component={Settings} />
-        </Switch>
-        <Footer />
-      </BrowserRouter>
+      <ThemeContextProvider>
+        <BrowserRouter>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={Splash} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/createaccount" component={CreateAccount} />
+            <Route exact path="/recipes" component={Recipes} />
+            <Route exact path="/account" component={Account} />
+            <Route exact path="/settings" component={Settings} />
+          </Switch>
+          <Footer />
+        </BrowserRouter>
+      </ThemeContextProvider>
     </AuthContextProvider>
   );
 }
