45804bd58a644003cad11f9a7f6e177d5ddf3004
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-05 19:11:15 -0400

    Update: Added context-based styling

--- src/components/ExternalLink/ExternalLink.tsx
+++ src/components/ExternalLink/ExternalLink.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import classnames from "classnames";
 import styles from "./ExternalLink.module.scss";
+
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 type ExternalLinkProps = {
   text: string;
@@ -10,18 +12,23 @@ type ExternalLinkProps = {
 };
 
 const ExternalLink = ({ text, icon, href }: ExternalLinkProps) => {
+  // Theme Context
+  const { isLightTheme, theme } = useContext(ThemeContext);
+  const LocalTheme = isLightTheme ? theme.light : theme.dark;
+
   return (
     <a
       href={href}
       className={classnames(
         "helpingMarginLR noWrap is-size-6",
         styles.textHighlight,
-        styles.hvrSweepToRight,
+        styles.hvrSweepToRight
       )}
       target="_blank"
       rel="noopener noreferrer"
+      style={{background: LocalTheme.buttonBackground}}
     >
-      <strong>
+      <strong style={{color: LocalTheme.syntax}}>
         {text} <i className={icon}></i>
       </strong>
     </a>
