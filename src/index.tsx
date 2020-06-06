851c46a1aa512b50be63eb8b34943efc743991e0
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-06 19:53:40 -0400

    Update: Added contexts from App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+// Contexts
+import AuthContextProvider from "./contexts/AuthContext";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <ThemeContextProvider>
+        <App />
+      </ThemeContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
