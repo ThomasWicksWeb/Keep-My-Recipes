01e8a79d5149049195389dc98e15b95d09e0e09c
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-06 19:53:29 -0400

    Refactor: Removed contexts from App.tsx to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,6 @@ import "bulma"; // Styles app on a global level
 import "./BulmaFixes.scss";
 import "react-toastify/dist/ReactToastify.css";
 
-// Contexts
-import AuthContextProvider from "./contexts/AuthContext";
-import ThemeContextProvider from "./contexts/ThemeContext";
-
 // Components
 import { Footer } from "./components/Footer";
 import { NavBar } from "./components/NavBar";
@@ -48,22 +44,18 @@ export const auth = firebase.auth();
 
 function App() {
   return (
-    <AuthContextProvider>
-      <ThemeContextProvider>
-        <BrowserRouter>
-          <NavBar />
-          <Switch>
-            <Route exact path="/" component={Splash} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/createaccount" component={CreateAccount} />
-            <Route exact path="/recipes" component={Recipes} />
-            <Route exact path="/account" component={Account} />
-            <Route exact path="/settings" component={Settings} />
-          </Switch>
-          <Footer />
-        </BrowserRouter>
-      </ThemeContextProvider>
-    </AuthContextProvider>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Splash} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/createaccount" component={CreateAccount} />
+        <Route exact path="/recipes" component={Recipes} />
+        <Route exact path="/account" component={Account} />
+        <Route exact path="/settings" component={Settings} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
