ebd531ce8ab13f884d1f2b8e019b1773306eaa07
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-09 21:07:25 -0400

    Update: Updated but broken

--- src/components/RecipeList/RecipeList.tsx
+++ src/components/RecipeList/RecipeList.tsx
@@ -1,7 +1,36 @@
 import React from "react";
 
-const RecipeList = () => {
-  return <p>RecipeList</p>;
+type RecipeListProps = {
+  AllRecipes: Array<object>;
+};
+
+const RecipeList = ({ AllRecipes }: RecipeListProps) => {
+
+  // EXAMPLE RECIPE OBJECT
+  // Title: "Ice Cream Sundae"
+  // Description: "A delicious ice cream and banana snack!"
+  // ingredients: "pepper, eggs, milk, red pepper"
+  // instructions: "Preheat so you can cook idiot"
+  // tags: "tasty, dessert, delicious"
+  // recipeID: 1234
+
+  console.log('--------')
+  console.log(AllRecipes)
+  console.log(typeof(AllRecipes))
+
+  const MappedRecipeList = AllRecipes.map((e) => {
+    return (
+      <li>
+        <p>hello {e}</p>
+      </li>
+    );
+  });
+
+  return (
+    <main>
+      <ul>{MappedRecipeList}</ul>
+    </main>
+  );
 };
 
 export default RecipeList;
