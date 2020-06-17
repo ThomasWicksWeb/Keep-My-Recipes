738b82291a545581603456c58bce415f1aa9651c
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-16 23:56:12 -0400

    Update: Now displays a recipe title based on the users selecting one from the list

--- src/components/SingleRecipe/SingleRecipe.tsx
+++ src/components/SingleRecipe/SingleRecipe.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
 
-const SingleRecipe = () => {
-  return <p>RecipeCard</p>;
+// Contexts
+import { ThemeContext } from "../../contexts/ThemeContext";
+
+// EXAMPLE RECIPE OBJECT
+// Title: "Ice Cream Sundae"
+// Description: "A delicious ice cream and banana snack!"
+// ingredients: "pepper, eggs, milk, red pepper"
+// instructions: "Preheat so you can cook idiot"
+// tags: "tasty, dessert, delicious"
+// recipeID: 1234
+
+type SingleRecipeProps = {
+  Recipe: {
+    Title: string;
+    Description: string;
+    ingredients: string;
+    instructions: string;
+    tags: string;
+    recipeID: string;
+  };
+  // AllRecipes: Array<object>;
+  AllRecipes: any;
+  RecipeID: string;
+};
+
+const SingleRecipe = ({ AllRecipes, RecipeID }: SingleRecipeProps) => {
+  const { isLightTheme, theme } = useContext(ThemeContext);
+  const LocalTheme = isLightTheme ? theme.light : theme.dark;
+
+  if (RecipeID !== null) {
+    const FoundRecipe = AllRecipes.find((e: any) => e.recipeID === RecipeID);
+    console.log("Found recipe is: ", FoundRecipe)
+    return <div>{FoundRecipe.Title}</div>;
+  } else {
+    return <>No item selected</>;
+  }
 };
 
 export default SingleRecipe;
