cbd189a23819197d8f7bc02876986da47f3c56ad
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-22 21:35:47 -0400

    Update SingleRecipe.tsx
    Added theme context to component

--- src/components/SingleRecipe/SingleRecipe.tsx
+++ src/components/SingleRecipe/SingleRecipe.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 
-import { SingleRecipeTemplate } from './SingleRecipeTemplate'
+import { SingleRecipeTemplate } from "./SingleRecipeTemplate";
 
 // Contexts
 import { ThemeContext } from "../../contexts/ThemeContext";
@@ -22,13 +22,23 @@ const SingleRecipe = ({ AllRecipes, RecipeID }: SingleRecipeProps) => {
   const { isLightTheme, theme } = useContext(ThemeContext);
   const LocalTheme = isLightTheme ? theme.light : theme.dark;
 
+  // If a recipe exists, find it and return it
   if (RecipeID !== null) {
     const FoundRecipe = AllRecipes.find((e: any) => e.recipeID === RecipeID);
 
     return <SingleRecipeTemplate Recipe={FoundRecipe} />;
-
   } else {
-    return <>No item selected</>;
+    // If no recipe exists, return placeholder
+    return (
+      <section>
+        <h1
+          className="has-text-weight-bold is-size-4 has-text-centered"
+          style={{ color: LocalTheme.syntax }}
+        >
+          No recipe selected
+        </h1>
+      </section>
+    );
   }
 };
 
