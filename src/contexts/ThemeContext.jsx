2e6646a8ecc8c4ba1ee883b941f77c5c414f5821
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-25 13:20:42 -0400

    Update ThemeContext.jsx

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -3,20 +3,26 @@ import React, { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext();
 
 const ThemeContextProvider = (props) => {
-  const [isLightTheme, setTheme] = useState(false);
+  const [isLightTheme, setTheme] = useState(true);
 
   const theme = {
     light: {
       syntax: "black",
       backgroundColorLight: "white",
-      backgroundColorDark: "#fafafa"
+      backgroundColorDark: "#fafafa",
+      "&:hover": {
+        underline: "#333"
+      }
     },
     dark: {
       syntax: "white",
       backgroundColorLight: "#1f2b46",
       backgroundColorDark: "#17223b",
       buttonBackground: "#e54242",
-      linkText: "lightskyblue"
+      linkText: "lightskyblue",
+      "&:hover": {
+        underline: "white"
+      }
     },
   };
 
